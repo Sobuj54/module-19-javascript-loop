664a610698d598c959e94d53cd96f48381cd4e24
sobuj <sobujahmed124@gmail.com>
2023-01-28 13:27:32 +0600

    array loops

---
+++ array_loop.js
@@ -0,0 +1,17 @@
+// indexing array with loop
+
+var numbers = [22, 25, 42, 45, 58, 78, 75, 98]
+var len = numbers.length
+// console.log(len)
+
+for (var i = 0; i < len; i++) {
+  console.log(numbers[i])
+}
+
+// string
+var names = ['notebook', 'pen', 'house', 'computer']
+var leng = names.length
+
+for (var i = 0; i < leng; i++) {
+  console.log(names[i])
+}
