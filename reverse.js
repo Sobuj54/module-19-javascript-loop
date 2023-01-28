75226895a36387ab29c2d0e754b78e58c7823182
sobuj <sobujahmed124@gmail.com>
2023-01-28 13:55:26 +0600

    reverse in loop

---
+++ reverse.js
@@ -0,0 +1,13 @@
+// starting loop from reverse
+// reverse in while loop
+console.log('reverse in while loop:')
+var num = 10
+while (num > 0) {
+  console.log(num)
+  num--
+}
+// reverse in for loop
+console.log('reverse for loop:')
+for (var i = 10; i > 0; i--) {
+  console.log(i)
+}
