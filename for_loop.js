62ba9e2e7e536892b722cb19d782418aa6114a60
sobuj <sobujahmed124@gmail.com>
2023-01-28 13:10:19 +0600

    for loop

---
+++ for_loop.js
@@ -0,0 +1,21 @@
+// for loop
+
+console.log('1 to 10 all numbers :')
+for (var i = 1; i <= 10; i++) {
+  console.log(i)
+}
+// all the even numbers from 1 to 10
+
+console.log('all the even numbers from 1 to 10:')
+for (var i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i)
+  }
+}
+// all the odd numbers form 1 to 10
+console.log('all the odd numbers from 1 to 10:')
+for (var i = 1; i <= 10; i++) {
+  if (i % 2 != 0) {
+    console.log(i)
+  }
+}
